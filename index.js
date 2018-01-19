81ea66162e080dbb00ab3129df14449286ce5be3
Sander Pick <sanderpick@gmail.com>
2018-01-19 13:02:25 -0800

    remove logging

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ var github = githubhook({path: '/gitlab/callback'});
 github.listen();
 
 github.on('push', function (repo, ref, data) {
-  console.log(repo, ref, data)
   if (REPO === data['project']['path_with_namespace'])
     mkdocs.build(data['project']['path_with_namespace']);
 });
@@ -33,7 +32,6 @@ github.on('push', function (repo, ref, data) {
 var app = express();
 
 app.post('/gitlab/callback', function(req, res) {
-  console.log(req)
   proxy.web(req, res, {
     target: 'http://127.0.0.1:3420'
   });
@@ -45,4 +43,3 @@ server.init(app);
 app.listen(process.env.PORT || 9090);
 
 console.log('Server started http://127.0.0.1:9090');
-
